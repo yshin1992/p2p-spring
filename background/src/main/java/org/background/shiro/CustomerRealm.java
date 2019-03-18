cf8dfd9b4ab125bc1ab6d11349b26f2ea05453b8
yshin1992 <yshin1992@163.com>
2019-03-18 18:08:34 +0800

    跟踪源码看Shiro的执行流程

--- background/src/main/java/org/background/shiro/CustomerRealm.java
+++ background/src/main/java/org/background/shiro/CustomerRealm.java
@@ -3,21 +3,51 @@ package org.background.shiro;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * 自定义
+ * @author yshin1992
+ * 域，Shiro 从从 Realm 获取安全数据（如用户、角色、权限），就是说 SecurityManager 要验证用户身份，
+ * 那么它需要从 Realm 获取相应的用户进行比较以确定用户身份是否合法；
+ * 也需要从 Realm 得到用户相应的角色 / 权限进行验证用户是否能进行操作；可以把 Realm 看成 DataSource，即安全数据源。
+ */
 public class CustomerRealm extends AuthorizingRealm {
 
+	private Logger logger = LoggerFactory.getLogger(CustomerRealm.class);
+	
+	/**
+	 * 获取授权信息
+	 */
 	@Override
-	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection arg0) {
-		return null;
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+		
+		
+		return new SimpleAuthorizationInfo();
 	}
 
+	/**
+	 * 获取身份验证信息
+	 */
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(
-			AuthenticationToken arg0) throws AuthenticationException {
-		return null;
+			AuthenticationToken token) throws AuthenticationException {
+	
+		UsernamePasswordToken upToken = (UsernamePasswordToken)token;
+		String username = upToken.getUsername();
+		char[] password = upToken.getPassword();
+		
+		logger.error("username = "+ username);
+		logger.error("password = " + new String(password));
+		
+		return new SimpleAuthenticationInfo(username, new String(password),getName());
 	}
 
 }
---
+++ background/src/test/java/org/background/shiro/ShiroTest2.java
@@ -0,0 +1,17 @@
+package org.background.shiro;
+
+import junit.framework.TestCase;
+
+import org.apache.shiro.config.Ini;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.junit.Test;
+
+public class ShiroTest2 extends TestCase {
+	
+	@Test
+	public void testJDBCRealm(){
+		
+	}
+	
+	
+}
